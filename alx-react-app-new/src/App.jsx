214edddb92667fa90ef172220ccc2befb98d6305
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-09 09:13:47 +0200

    Add files via upload

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+       <div>
+        <Counter />
+      </div>
 
     </div>
 
