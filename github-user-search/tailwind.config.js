7dc5e5b18c8c23cca0e3944188a84dc6bad27e75
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-18 11:10:11 +0200

    Update tailwind.config.js

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // <-- include all React components
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
