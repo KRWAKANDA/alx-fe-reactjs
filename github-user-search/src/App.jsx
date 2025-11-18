3744bbc71af590dba4c538044ec49c9c3d7c83d1
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-18 10:45:20 +0200

    Add files via upload

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>GitHub User Search</h1>
+      <p>Setup complete. Start building your components.</p>
+    </div>
+  );
+}
+
+export default App;
