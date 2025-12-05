54b994ecf6c7978d98eefa65d068b8c6a8931181
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-12-05 10:34:16 +0200

    Add files via upload

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -8,10 +8,11 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind v4 is working!
-      </h1>
+    <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind v4 is working!
+    </h1>
 
+      
     </>
   )
 }
