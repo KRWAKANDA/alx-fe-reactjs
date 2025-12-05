b175600eea4af857d0ba7ddcdfbdb4120afaf37c
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-12-05 10:59:04 +0200

    Add files via upload

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <h1 className="text-4xl font-bold text-blue-500">
-      Tailwind v4 is working!
-    </h1>
-
-      
-    </>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
